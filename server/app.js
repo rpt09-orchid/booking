fb3c13b31882df49db5f760550cde2ad43604aa5
Jonathan Owens <jo@jondionowens.com>
2019-01-18 23:17:04 -0800

    Just starting to look through the app

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
 const path = require('path')
 const db = require('./database/db');
 const cors = require('cors');
--- server/database/db.js
+++ server/database/db.js
@@ -1,23 +1,28 @@
 const mongoose = require('mongoose');
+var test = process.env.NODE_ENV;
 let mongo_uri;
+console.log(process.env.NODE_ENV === 'development')
+
 // Loading DB
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV.trim() === 'development') {
+  console.log('poop')
   const keys = require('../config/keys')
   mongo_uri = keys.mongodbUri
+  console.log('mongo_uri')
 } else {
   mongo_uri = process.env.mongo_uri
 }
 
 
-mongoose.connect(mongo_uri, { useNewUrlParser: true });
+// mongoose.connect(mongo_uri, { useNewUrlParser: true });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-// Connecting to DB
-db.on('error', function() {
-  console.log('mongoose connection error');
-});
+// // Connecting to DB
+// db.on('error', function() {
+//   console.log('mongoose connection error');
+// });
 
-db.once('open', function() {
-  console.log('mongoose connected successfully');
-});
+// db.once('open', function() {
+//   console.log('mongoose connected successfully');
+// });

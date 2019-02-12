475ba1196f57929b473ce7078fcf60fbcb46e932
Jonathan Owens <jo@jondionowens.com>
2019-02-12 00:17:21 -0800

    Fixed a bunch of merge conflicts

--- client/src/components/CancelBooking.js
+++ client/src/components/CancelBooking.js
@@ -47,15 +47,9 @@ import axios from 'axios';
         <div id="cancel-booking-wrapper">
           <ul>
             {
-<<<<<<< HEAD
-               this.state.dates.map(function(item, i){
-                console.log(item.toString().slice(0, 11));
-                return <CancelBookingRow key={uniqid()} handleCancelDate={this.props.handleCancelDate} />
-=======
                this.state.dates.map((date) => {
                  bookingIdReference++;
                 return <CancelBookingRow key={uniqid()} date={date} bookingId={bookingIdReference - 1} handleCancelDate={this.handleCancelDate.bind(this)}/>
->>>>>>> bb456576b7eaf414c9db89837dee61a5b92aaff2
               })
             }
           </ul>
--- client/src/components/CancelBookingRow.js
+++ client/src/components/CancelBookingRow.js
@@ -6,13 +6,8 @@ const CancelBookingRow = (props) => {
   return (
     <li className="cancel-booking-row">
       <ul className="cancel-booking-row-elements">
-<<<<<<< HEAD
-        <li className="cancel-btn"><img src={closeBtn} onClick={this.props.handleCancelDate}/></li>
-        <li className="date-text">11-13-2019</li>
-=======
         <li className="cancel-btn"><img src={closeBtn} onClick={props.handleCancelDate}/></li>
         <li className="date-text">{props.date}</li>
->>>>>>> bb456576b7eaf414c9db89837dee61a5b92aaff2
       </ul>
     </li>
   )
